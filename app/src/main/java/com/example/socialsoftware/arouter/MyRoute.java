2d95c4bb1c5d7d485c4df8ef22ecb04e41149a88
z960320 <xiaozhang_909@163.com>
2020-10-23 12:40:56 +0900

    SocialDemo

---
+++ app/src/main/java/com/example/socialsoftware/arouter/MyRoute.java
@@ -0,0 +1,7 @@
+package com.example.socialsoftware.arouter;
+
+public class MyRoute {
+    public static final  String MAIN_ACTIVITY = "/activity/main";
+    public static final  String BINDING_ACTIVITY = "/activity/binding";
+}
+
